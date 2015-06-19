01783efbb0c5ccc6df5a940bedb1d27d619323c1
trong-lam.phan <trong-lam.phan@seitosei.fr>
2015-06-19 18:44:01 +0200

    finish all examples

--- app.py
+++ app.py
@@ -16,20 +16,59 @@ COLLECTION_NAME = 'projects'
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+	return render_template("index.html")
 
 @app.route("/maladie/projects/nb_malades_code")
 def maladie_projects():
-    connection     = MongoClient(MONGODB_HOST, MONGODB_PORT)
-    db             = connection[DBS_NAME];
-    collection     = db[COLLECTION_NAME]
-    code_activites = collection.find().distinct("Lieu_activite")
-    json_result    = []
-    for code_activite in code_activites:
-        json_result.append({code_activite: collection.count({"Lieu_activite": code_activite})})
-    json_result = json.dumps(json_result, default = json_util.default)
-    connection.close()
-    return json_result
+	connection     = MongoClient(MONGODB_HOST, MONGODB_PORT)
+	db             = connection[DBS_NAME];
+	collection     = db[COLLECTION_NAME]
+	code_activites = collection.find().distinct("Lieu_activite")
+	json_result    = []
+	for code_activite in code_activites:
+		json_result.append({code_activite: collection.count({"Lieu_activite": code_activite})})
+	json_result = json.dumps(json_result, default = json_util.default)
+	connection.close()
+	return json_result
+
+@app.route("/maladie/projects/nb_malades_age")
+def maladie_projects_age():
+	connection     = MongoClient(MONGODB_HOST, MONGODB_PORT)
+	db             = connection[DBS_NAME];
+	collection     = db[COLLECTION_NAME]
+	ages           = collection.find().distinct("Age")
+	json_result    = []
+	for age in ages:
+		json_result.append({age: collection.count({"Age": age})})
+	json_result = json.dumps(json_result, default = json_util.default)
+	connection.close()
+	return json_result
+
+@app.route("/maladie/projects/nb_malades_civilite")
+def maladie_projects_civilite():
+	connection     = MongoClient(MONGODB_HOST, MONGODB_PORT)
+	db             = connection[DBS_NAME];
+	collection     = db[COLLECTION_NAME]
+	sexes          = collection.find().distinct("Sexe")
+	json_result    = []
+	for sexe in sexes:
+		json_result.append({sexe: collection.count({"Sexe": sexe})})
+	json_result = json.dumps(json_result, default = json_util.default)
+	connection.close()
+	return json_result
+
+@app.route("/maladie/projects/nb_malades_metier")
+def maladie_projects_metier():
+	connection     = MongoClient(MONGODB_HOST, MONGODB_PORT)
+	db             = connection[DBS_NAME];
+	collection     = db[COLLECTION_NAME]
+	metiers        = collection.find().distinct("Profession")
+	json_result    = []
+	for metier in metiers:
+		json_result.append({metier: collection.count({"Profession": metier})})
+	json_result = json.dumps(json_result, default = json_util.default)
+	connection.close()
+	return json_result
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000,debug=True)
+	app.run(host='0.0.0.0',port=5000,debug=True)
--- static/custom/graphs.js
+++ static/custom/graphs.js
@@ -3,14 +3,14 @@ var width = 700;
 var height = 700;
 var path = d3.geo.path();
 var projection = d3.geo.conicConformal()
-	  				.center([4.638707, 45.904212])
-	  				.scale(50000)
-  					.translate([width / 2, height / 2]);
+					.center([4.638707, 45.904212])
+					.scale(50000)
+					.translate([width / 2, height / 2]);
 path.projection(projection);
 
 var svg = d3.select('#map').append("svg")
 			.attr("width", width)
-  			.attr("height", height);
+			.attr("height", height);
 
 var arrondissement = svg.append("g").attr("id", "departements");
 
@@ -39,9 +39,107 @@ $.get('maladie/projects/nb_malades_code', function(data) {
 					});
 					return result;
 				})
-		  		.attr("d", path)
-		  		.attr("onclick", "handler_zone_click($(this))")
+				.attr("d", path)
+				.attr("onclick", "handler_zone_click($(this))")
 	});
+});
+
+$.get('maladie/projects/nb_malades_age', function(data) {
+	data = JSON.parse(data);
+	var ages = [];
+	data.sort(function(a, b) {
+		var key_a, key_b;
+		for (k in a)
+			key_a = parseInt(k);
+		for (k in b)
+			key_b = parseInt(k);
+		if (key_a < key_b)
+			return -1;
+		else if (key_a > key_b)
+			return 1;
+		else
+			return 0;
+	});
+	data.forEach(function(obj) {
+		for(k in obj)
+			ages[k] = obj[k];
+	});
+	console.log(ages);
+	var count = 0;
+	d3.select("#age-stat")
+		.selectAll("div")
+			.data(ages)
+			.enter().append("div")
+				.style("height", function(d) { return d*10 + "px"; })
+				.style("margin-right", "1px")
+				.style("width", "13px")
+				.style("padding-top", function(d) {
+					if (d > 0)
+						return (d*10) + "px";
+					else
+						return "0";
+				})
+				.style("background-color", function(d) {
+					if (d > 0)
+						return "red";
+					else
+						return "white";
+				})
+				.attr("name", function(d) {
+					return count++;
+				})
+				.text(function(d) {
+					if (d > 0)
+						return d + "";
+					else
+						return "0";
+				});
+});
+
+$.get('maladie/projects/nb_malades_civilite', function(data) {
+	data = JSON.parse(data);
+	var sexes = [];
+	data.forEach(function(obj) {
+		for(k in obj)
+			sexes[k] = obj[k];
+	});
+	d3.select("#civilite-stat")
+		.selectAll("div")
+			.data(sexes)
+			.enter().append("div")
+				.style("width", function(d) { return d + "px"; })
+				.attr("name", function(d) {
+					if (d == sexes[1])
+						return "homme";
+					else
+						return "femme";
+				})
+				.text(function(d) { return d; });
+});
+
+$.get('maladie/projects/nb_malades_metier', function(data) {
+	data = JSON.parse(data);
+	metiers_arr = ['police', 'developper', 'manager', 'ouvrier', 'designer', 'consultant'];
+	var metiers = [];
+	var count = 0;
+	data.forEach(function(obj) {
+		for(k in obj)
+			metiers[k-1] = obj[k];
+	});
+	d3.select("#metier-stat")
+		.selectAll("div")
+			.data(metiers)
+			.enter().append("div")
+				.style("width", function(d) { return d*2 + "px"; })
+				.style("margin-bottom", "2px")
+				.attr("name", function(d) {
+					for (k in metiers)
+					{
+						if (d == metiers[k])
+							return metiers_arr[k];
+					}
+				})
+				.text(function(d) { return d; });
 });
 
 function handler_zone_click(obj) {
